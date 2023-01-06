ebfa153dae6a2c8156a3941ba05dc593ce6dfda7
Aesth <92583956+Ae-sth@users.noreply.github.com>
2023-01-06 14:45:35 +0000

    remains only one support to do

--- TODO.md
+++ TODO.md
@@ -5,5 +5,6 @@
 - [x] Support for argumentless handlers.
 
 # PHASE 1 - add support for multiple ways you can provide arguments to handlers
-- [ ] Support for position-based handlers(1 argument).
-- [ ] Support for scene-stack arguments.
+- [x] Support for position-based handlers(1 argument).
+- [x] Support for scene-stack arguments.
+- [ ] Support for listable arguments.
--- example/setup.ts
+++ example/setup.ts
@@ -1,48 +1,50 @@
 import { RadialMenu } from "../src/core/menu.js"
 import { GeometryN } from "../src/interfaces/geometry.js"
 import { RadialMenuN } from "../src/interfaces/menu.js"
-import Scene from "../src/misc/scene.js"
+import Scene from "./src/misc/scene.js"
 import Icon from "../src/utils/icon.js"
 
 export default async function setup(context: CanvasRenderingContext2D){
     
     Scene.bind(context)
     RadialMenu.bind(Scene)
-
-
+    
     const radialMenuConfig: RadialMenuN.ConfigT = [
 
-         {
-            label: "equal",
-            icon: Icon.plus,
-            handler: function(){console.log("equal")},
-            handlerType: "ARGUMENTLESS"
-        },       {
-            label: "add",
-            handler: function(pos: any){console.log(pos)},
-            handlerType: "POSITION-BASED"
-        },
         {
-            label: "more",
-            subOptions: [
-                {
-                    label: "multiply",
-                    handler: function(args: any){console.log(args)},
-                    handlerType: "STACK-BASED"
-                },
-                {
-                    label: "division",
-                    handler: function(){console.log("division")},
-                    handlerType: "ARGUMENTLESS"
-                }
-            ]
-        }
-    ] 
-    RadialMenu.load(radialMenuConfig)
+           label: "equal",
+           icon: Icon.plus,
+           handler: function(){console.log("equal")},
+           handlerType: "ARGUMENTLESS"
+       },       {
+           label: "add",
+           handler: function(pos: any){console.log(pos)},
+           handlerType: "POSITION-BASED"
+       },
+       {
+           label: "more",
+           subOptions: [
+               {
+                   label: "multiply",
+                   handler: function(...args: any){console.log(args)},
+                   handlerType: "STACK-BASED"
+               },
+               {
+                   label: "division",
+                   handler: function(){console.log("division")},
+                   handlerType: "ARGUMENTLESS"
+               }
+           ]
+       }
+   ] 
+   RadialMenu.load(radialMenuConfig)
+
+
+
+
     
    
     Scene.render()
-    
     context.canvas.addEventListener("mousedown", handleClickEvent, false)
     function handleClickEvent(event: MouseEvent){
         const eventPosition = [event.clientX-Scene.cursor_offset, event.clientY-Scene.cursor_offset] as GeometryN.PointT
